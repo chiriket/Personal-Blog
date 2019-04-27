885d54ead4c94bc08dec6895b43771653cda4d05
Shirley Keter <shirleyketer@gmail.com>
2019-04-27 18:36:46 +0300

    create user class

--- config.py
+++ config.py
@@ -2,8 +2,32 @@ import os
 
 
 class Config:
+    '''
+    General configuration parent class
+    '''
 
     QUOTES_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    QUOTES_API_KEY = os.environ.get('MOVIE_API_KEY')
+    QUOTES_API_KEY = os.environ.get('QUOTES_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/watchlist'
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
