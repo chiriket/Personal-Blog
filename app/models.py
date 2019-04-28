21f4680bf935b04ce344c9235c817a5b5f0d014f
Shirley Keter <shirleyketer@gmail.com>
2019-04-28 11:54:42 +0300

    added bootsrap

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(48),unique=True, index = True)
+    hash_pass = db.Column(db.String(255)) 
 
     def __repr__(self):
         return f'User {self.username}'
---
+++ manage.py
@@ -0,0 +1,31 @@
+
+from app import create_app, db
+from flask_script import Manager, Server
+# from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Blog, Email
+
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app, db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db=db, User = User, Blog = Blog, Email = Email)
+
+@manager.command
+def test():
+    '''
+    Runs the unittest
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__=='__main__':
+    manager.run()
