efc81041d71fa29520b1ac3b4c27a6869a85e20b
Shirley Keter <shirleyketer@gmail.com>
2019-04-28 12:02:26 +0300

    added datetime

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from . import db
-from . import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+# from . import db, login_manager
 
 #...
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
+class User(UserMixin,db.Model)
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(48), unique = True, index=True)
