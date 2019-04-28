a3d6a6adc6d1d325c2168ffc6c90876e25ea708c
Shirley Keter <shirleyketer@gmail.com>
2019-04-28 13:32:45 +0300

    added mail

--- app/models.py
+++ app/models.py
@@ -104,3 +104,14 @@ class Email(db.Model):
     def send_single_email(cls,id):
         email = Email.query.filter_by(id=id).first()
         return email
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
